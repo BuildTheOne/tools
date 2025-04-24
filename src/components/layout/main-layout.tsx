443f6b797bb78fb0cc620a030f4dac2875fdfe5f
Ignatius Henriyanto Primai Renda <76039582+BuildTheOne@users.noreply.github.com>
2025-04-24 23:27:04 +0700

    fix: gh pages config

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,17 +1,17 @@
 import AppLayout from "@/components/layout/app-layout";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const MainLayout = async ({ children }: LayoutProps) => {
-  const cookie = await cookies();
-  const defaultOpen = cookie.get("sidebar_state")?.value === "true";
+  // const cookie = await cookies();
+  // const defaultOpen = cookie.get("sidebar_state")?.value === "true";
 
   return (
-    <SidebarProvider defaultOpen={defaultOpen}>
+    <SidebarProvider defaultOpen={true}>
       <AppLayout>{children}</AppLayout>
     </SidebarProvider>
   );
